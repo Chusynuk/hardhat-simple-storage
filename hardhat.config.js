cee034843723523388a46c297591f0e517a1cdad
Eloi <recycled_loops@hotmail.com>
2022-10-18 18:03:54 +0200

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("dotenv").config()
+require("@nomicfoundation/hardhat-toolbox")
+require("@nomiclabs/hardhat-etherscan")
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
+  solidity: "0.8.7",
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+}
