aeea70c7138728b8454e054e59575eaa4aaee0ea
Eloi <recycled_loops@hotmail.com>
2022-10-20 16:29:17 +0200

    use hardhat-gas-reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,8 @@
 require("dotenv").config()
 require("@nomicfoundation/hardhat-toolbox")
 require("@nomiclabs/hardhat-etherscan")
+require("./tasks/block-number")
+require("hardhat-gas-reporter")
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
@@ -15,9 +17,17 @@ module.exports = {
       accounts: [PRIVATE_KEY],
       chainId: 5,
     },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+      // accounts: Hardhat already took care of it
+    },
   },
   solidity: "0.8.7",
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
   },
 }
